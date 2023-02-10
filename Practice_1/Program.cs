cd8f5087cf966924e31d08a4a0ad4c681215a51b
Juno <polya.alexei@yandex.ru>
2023-02-10 10:33:13 +0300

    add folder Practice_1

---
+++ Practice_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+//на само себя).
+
+//Например:
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+
+string example = Console.ReadLine(); 
+int a = Convert.ToInt32(example);                                 //Convert.ToInt32(string)   string example = Console.ReadLine()  int a = Convert.ToInt32(example)
+                                                
+//int result = 1;
+
+int result = a * a;
+Console.WriteLine(result);
+
+
