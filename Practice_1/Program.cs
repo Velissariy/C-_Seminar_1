0e21e6e7388e28365d27b7011906e5261b8290d6
Juno <polya.alexei@yandex.ru>
2023-02-10 10:36:19 +0300

    update Practice_1

--- Practice_1/Program.cs
+++ Practice_1/Program.cs
@@ -14,4 +14,10 @@ int a = Convert.ToInt32(example);                                 //Convert.ToIn
 int result = a * a;
 Console.WriteLine(result);
 
+//Console.WriteLine("Введите число");
+//string example = Console.ReadLine();
+//int a = Convert.ToInt32(example);
 
+//Console.WriteLine("Вывод:");
+//int result = a * a;
+//Console.WriteLine(result);
