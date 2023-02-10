c7bb61b82f08f4c8db385866530fa5902a19c800
Juno <polya.alexei@yandex.ru>
2023-02-10 12:19:48 +0300

    add folder Practice_5

---
+++ Practice_5/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+
+
+Console.WriteLine("Введите число");
+string example = Console.ReadLine();
+int a = Convert.ToInt32(example);
+int result = a % 10;
+Console.WriteLine(result);
+
+
+// Console.WriteLine("Введите число");
+// string example = Console.ReadLine();
+// int a = Convert.ToInt32(example);
+// int result = a % 10;
+// Console.WriteLine(result);
