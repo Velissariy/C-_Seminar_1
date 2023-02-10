48edb9cc86dba2447e5a80db64c696ba06ce28ef
Juno <polya.alexei@yandex.ru>
2023-02-10 11:31:39 +0300

    change Practice_2 and Practice_3

--- Practice_2/Program.cs
+++ Practice_2/Program.cs
@@ -36,24 +36,14 @@ System.Console.ReadLine();
 }
 
 
-//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-//3 -> Среда 
-//5 -> Пятница
-
-System.Console.WriteLine("Введите день недели");
-int a = Convert.ToInt32(System.Console.ReadLine());
-
-if (a ==1)
-System.Console.WriteLine("День недели:понедельник");
-else if (a ==2)
-System.Console.WriteLine("День недели:вторник");
 
 
 
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
 
-if (a*a==b)
-{Console.WriteLine("True");}
-else
-Console.WriteLine("False");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
+
+//if (a*a==b)
+//{Console.WriteLine("True");}
+//else
+//Console.WriteLine("False");
